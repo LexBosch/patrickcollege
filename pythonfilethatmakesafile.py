5c5780a2d67148443fcea9dd7a1a5178eaa30e4a
Lex Bosch <31917165+LexBosch@users.noreply.github.com>
2020-02-17 14:47:35 +0100

    Update pythonfilethatmakesafile.py

--- pythonfilethatmakesafile.py
+++ pythonfilethatmakesafile.py
@@ -4,18 +4,19 @@ import random
 
 def main():
     Entrez.email = "lexbosch@live.nl"  # Always tell NCBI who you are
-    random_amount_of_data = [str(random.randrange(22000000, 32000000)) for x in range(0, random.randrange(1400, 1600))]
+    random_amount_of_data = list(set([str(random.randrange(12000000, 32000000)) for x in range(0, 100000)]))
     handle = Entrez.efetch(db="pubmed", id=random_amount_of_data, rettype="medline", retmode="xml")
     reed_data = Entrez.read(handle)
 
-    new_file = open("bestandsnaam.txt", "a")
+    new_file = open("bestandsnaam.txt", "w")
     lelijke_counter = 0
 
     for paper in reed_data["PubmedArticle"]:
         try:
             maybe_abstract = (paper["MedlineCitation"]["Article"]["Abstract"]["AbstractText"][0])
             if maybe_abstract:
-                new_file.write(maybe_abstract + "\n")
+                if not ">" in maybe_abstract:
+                    new_file.write(maybe_abstract + "\n")
         except Exception:
             lelijke_counter -= -1
             print(lelijke_counter)
